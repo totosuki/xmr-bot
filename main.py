d3fd3181e552c83ea53c6737c9300bf1c1e2748a
totosuki <e231kari@gmail.com>
2024-06-12 13:58:34 +0900

    関数の位置を調整 & test関数を削除

--- main.py
+++ main.py
@@ -20,18 +20,6 @@ async def test(interaction: discord.Interaction):
     await interaction.response.send_message('Hello, World!')
 
 @tree.command(
-    name="xmr-help",
-    description="XMRボットのヘルプを表示する"
-)
-async def xmr_help(interaction: discord.Interaction):
-    await interaction.response.defer()
-    embed = discord.Embed(title="XMRボットのヘルプ", description="XMRボットのコマンド一覧です", color=config.GREEN)
-    embed.add_field(name="xmr-balance", value="現在のXMR残高を日本円で表示する", inline=False)
-    embed.add_field(name="xmr-hashrate hour(default=2)", value="平均ハッシュレートを表示する\n`hour = 0` にすると現在のハッシュレートを表示する", inline=False)
-    embed.add_field(name="xmr-help", value="XMRボットのヘルプを表示する", inline=False)
-    await interaction.followup.send(embed=embed)
-
-@tree.command(
     name = "xmr-balance",
     description = "現在のXMR残高を日本円で表示する"
 )
@@ -52,6 +40,18 @@ async def xmr_hashrate(interaction: discord.Interaction, hour: int = 2):
         embed = discord.Embed(title="XMRハッシュレート", description=f"過去{hour}時間の平均ハッシュレートは{hashrate:.2f}H/sです", color=config.GREEN)
     else:
         embed = discord.Embed(title="XMRハッシュレート", description=f"現在のハッシュレートは{hashrate:.2f}H/sです", color=config.GREEN)
+    await interaction.followup.send(embed=embed)
+
+@tree.command(
+    name="xmr-help",
+    description="XMRボットのヘルプを表示する"
+)
+async def xmr_help(interaction: discord.Interaction):
+    await interaction.response.defer()
+    embed = discord.Embed(title="XMRボットのヘルプ", description="XMRボットのコマンド一覧です", color=config.GREEN)
+    embed.add_field(name="xmr-balance", value="現在のXMR残高を日本円で表示する", inline=False)
+    embed.add_field(name="xmr-hashrate hour(default=2)", value="平均ハッシュレートを表示する\n`hour = 0` にすると現在のハッシュレートを表示する", inline=False)
+    embed.add_field(name="xmr-help", value="XMRボットのヘルプを表示する", inline=False)
     await interaction.followup.send(embed=embed)
 
 client.run(config.TOKEN)
