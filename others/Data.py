2b2b901359c1a7e2cd9fda2390d3a5b523b7507d
totosuki <e231kari@gmail.com>
2024-10-11 12:07:34 +0900

    各コマンドのクラスを作成

---
+++ others/Data.py
@@ -0,0 +1,24 @@
+class Address:
+    name = "xmr-address"
+    title = "XMR Address"
+    description = "設定されているXMRのアドレスを表示します"
+
+class Balance:
+    name = "xmr-balance"
+    title = "XMR Balance"
+    description = "現在のXMR残高を日本円で表示します"
+
+class Hashrate:
+    name = "xmr-hashrate"
+    title = "XMR Hashrate"
+    description = "平均ハッシュレートを表示する hour = 0 にすると現在のハッシュレートを表示する"
+
+class Help:
+    name = "xmr-help"
+    title = "XMR Hashrate"
+    description = "XMRボットのヘルプを表示する"
+
+class Member:
+    name = "xmr-member"
+    title = "XMR Member"
+    description = "現在マイニング中のメンバーを表示する"
