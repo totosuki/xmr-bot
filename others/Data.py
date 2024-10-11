cf2cbba16d556dd926efb8e10b87db5ba45866a2
totosuki <e231kari@gmail.com>
2024-10-11 12:17:53 +0900

    Hashrateクラスにembed_description関数を作成

--- others/Data.py
+++ others/Data.py
@@ -12,6 +12,16 @@ class Hashrate:
     name = "xmr-hashrate"
     title = "XMR Hashrate"
     description = "平均ハッシュレートを表示する hour = 0 にすると現在のハッシュレートを表示する"
+    def embed_description(hour: int, member: str, hashrate: float | int):
+        if member:
+            if hashrate:
+                return f"{member}のハッシュレートは{hashrate}H/sです"
+            else:
+                return f"{member}はマイニングしていません"
+        elif hour:
+            return f"過去{hour}時間の平均ハッシュレートは{hashrate:.2f}H/sです"
+        else:
+            return f"現在のハッシュレートは{hashrate:.2f}H/sです"
 
 class Help:
     name = "xmr-help"
